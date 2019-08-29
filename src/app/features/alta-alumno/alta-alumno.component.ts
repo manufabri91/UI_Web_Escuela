fcad7500bc5c4e7aee000acc534cf6c977237ece
yamir_132 <yamir_132@hotmail.com>
2019-08-28 22:06:58 -0300

    Create alumno form

--- src/app/features/alta-alumno/alta-alumno.component.ts
+++ src/app/features/alta-alumno/alta-alumno.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IAlumno } from 'src/app/shared/IAlumno';
+import { AlumnoService } from 'src/app/core/services/alumno.service';
+import { Alumno } from 'src/app/core/models/alumno';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-alta-alumno',
@@ -8,24 +12,52 @@ import { IAlumno } from 'src/app/shared/IAlumno';
 })
 export class AltaAlumnoComponent implements OnInit {
   alumno : IAlumno;
- 
-  constructor(
-    // private postService : PostService
-    ) { }
+  alumnoDto : Alumno;
 
+  constructor(private alumnoService : AlumnoService) { }
+
+  alumnoForm = new FormGroup({
+    legajo: new FormControl('',[
+      Validators.required,
+      Validators.maxLength(25)
+    ]),
+    nombre: new FormControl('',[
+      Validators.required,
+      Validators.maxLength(25)
+    ]),
+    apellido: new FormControl('',[
+      Validators.required,
+      Validators.maxLength(25)
+    ]),
+    nacimiento: new FormControl('',[
+      Validators.required,
+      Validators.maxLength(25)
+    ]),
+    dni: new FormControl('',[
+      Validators.required,
+      Validators.maxLength(25)
+    ]),
+    telefono: new FormControl('',[
+      Validators.required,
+      Validators.maxLength(25)
+    ])
+  });
   ngOnInit() {
-      this.alumno = {
-        nombre : '',
-        apellido : '',
-        nacimiento : '',
-        dni : '',
-        telefono : ''
-      };
+
   }
+
+  get legajo() { return this.alumnoForm.get('legajo'); }
+  get nombre() { return this.alumnoForm.get('nombre'); }
+  get apellido() { return this.alumnoForm.get('apellido'); }
+  get nacimiento() { return this.alumnoForm.get('nacimiento'); }
+  get dni() { return this.alumnoForm.get('dni'); }
+  get telefono() { return this.alumnoForm.get('telefono'); }
+
   submitted = false;
 
   onSubmit() { 
-  this.submitted = true;
+    this.submitted = true;
+    this.alumnoService.cargarAlumnos(this.alumnoForm.value);
   }
 
 }
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CareerDetailComponent } from './career-detail/career-detail.component';
 import { AltaAlumnoComponent } from './alta-alumno/alta-alumno.component';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CareerDetailComponent, AltaAlumnoComponent],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [CareerDetailComponent]
 })
